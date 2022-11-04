34d328a9773a6eafee84aa32ab9e0d175c097ff2
mariusor <marius@federated.id>
2022-11-04 16:40:13 +0100

    Small improvements to the errors returned

--- errors.go
+++ errors.go
@@ -2,40 +2,53 @@ package client
 
 import (
 	"fmt"
+	"io"
 
 	vocab "github.com/go-ap/activitypub"
 )
 
 type err struct {
-	errs []error
-	msg  string
-	i    vocab.IRI
+	err error
+	msg string
+	i   vocab.IRI
 }
 
-func (e *err) annotate(errs ...error) *err {
-	e.errs = errs
+func (e err) annotate(err error) err {
+	e.err = err
 	return e
 }
 
-func (e *err) iri(i vocab.IRI) *err {
+func (e err) iri(i vocab.IRI) err {
 	e.i = i
 	return e
 }
 
-func errf(msg string, p ...interface{}) *err {
-	return &err{
+func errf(msg string, p ...interface{}) err {
+	return err{
 		msg: fmt.Sprintf(msg, p...),
 	}
 }
 
 // Error returns the formatted error
-func (e *err) Error() string {
+func (e err) Error() string {
 	return e.msg
 }
 
-func (e *err) Unwrap() error {
-	if len(e.errs) == 0 {
-		return nil
+func (e err) Unwrap() error {
+	return e.err
+}
+
+func (e err) Format(s fmt.State, verb rune) {
+	switch verb {
+	case 's', 'v':
+		io.WriteString(s, e.msg)
+		switch {
+		case s.Flag('+'):
+			if e.err == nil {
+				return
+			}
+			io.WriteString(s, ": ")
+			io.WriteString(s, fmt.Sprintf("%+s", e.err))
+		}
 	}
-	return e.errs[0]
 }
