26e3111da3d7e8ca553a7df5e3bb16df7bc895e3
Marius Orcsik <marius@federated.id>
2025-01-26 16:39:14 +0100

    A possible mitigation for #301
    We now try to fallback to unsigned requests when we receive an error
    for a HTTPSignatureTransport.Base.RoundTrip.

--- s2s/httpsignatures.go
+++ s2s/httpsignatures.go
@@ -36,11 +36,38 @@ func S2SWrapTransport(base http.RoundTripper, actor *vocab.Actor, key crypto.Pri
 	}
 }
 
+// RoundTrip dispatches the received request after signing it
 func (s *HTTPSignatureTransport) RoundTrip(req *http.Request) (*http.Response, error) {
+	or := req
+	attemptUnauthorized := false
+
 	if s.Actor != nil {
+		req = cloneRequest(or) // per RoundTripper contract
 		_ = s.signRequest(req)
+
+		// NOTE(marius): for GET/HEAD requests we should try again without the authorization header
+		// if the RoundTrip fails to produce a response.
+		attemptUnauthorized = !(req.Method == http.MethodGet || req.Method == http.MethodHead)
 	}
-	return s.Base.RoundTrip(req)
+
+	res1, err := s.Base.RoundTrip(req)
+	if !attemptUnauthorized || err == nil {
+		return res1, err
+	}
+
+	// NOTE(marius): if we received an actual error we try the request again, but unsigned.
+	//
+	// This is a pretty hacky mitigation for loading Public Keys for Actors on other instances, which
+	// sometimes triggers a feedback loop if the instance tries to authorize the signing actor in its turn.
+	//
+	// I detailed that faulty behaviour in ticket:
+	//  https://todo.sr.ht/~mariusor/go-activitypub/301
+	req = cloneRequest(or) // per RoundTripper contract
+	res2, err := s.Base.RoundTrip(req)
+	if err != nil {
+		return res1, err
+	}
+	return res2, nil
 }
 
 func (s *HTTPSignatureTransport) signRequest(req *http.Request) error {
@@ -85,3 +112,17 @@ func (s *HTTPSignatureTransport) signRequest(req *http.Request) error {
 }
 
 var _ http.RoundTripper = new(HTTPSignatureTransport)
+
+// cloneRequest returns a clone of the provided *http.Request.
+// The clone is a shallow copy of the struct and its Header map.
+func cloneRequest(r *http.Request) *http.Request {
+	// shallow copy of the struct
+	r2 := new(http.Request)
+	*r2 = *r
+	// deep copy of the Header
+	r2.Header = make(http.Header, len(r.Header))
+	for k, s := range r.Header {
+		r2.Header[k] = append([]string(nil), s...)
+	}
+	return r2
+}
