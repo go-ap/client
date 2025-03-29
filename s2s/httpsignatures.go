a930af84fb25ce64d1af1b39a35d25ba3c027feb
Marius Orcsik <marius@federated.id>
2025-03-29 15:21:34 +0100

    Improve errors for HTTP Signature Round Tripper
    We now double check that the Actor's public key matches the private one we're trying to sign with

--- s2s/httpsignatures.go
+++ s2s/httpsignatures.go
@@ -6,6 +6,9 @@ import (
 	"crypto/ecdsa"
 	"crypto/ed25519"
 	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
+	"fmt"
 	"io"
 	"net/http"
 	"time"
@@ -40,18 +43,21 @@ func (s *HTTPSignatureTransport) RoundTrip(req *http.Request) (*http.Response, e
 	or := req
 	attemptUnauthorized := false
 
+	var signErr error
 	if s.Actor != nil {
 		req = cloneRequest(or) // per RoundTripper contract
-		_ = s.signRequest(req)
+		if err := s.signRequest(req); err != nil {
+			signErr = fmt.Errorf("unable to sign request: %w", err)
+		}
 
 		// NOTE(marius): for GET/HEAD requests we should try again without the authorization header
 		// if the RoundTrip fails to produce a response.
-		attemptUnauthorized = !(req.Method == http.MethodGet || req.Method == http.MethodHead)
+		attemptUnauthorized = req.Method == http.MethodGet || req.Method == http.MethodHead
 	}
 
 	res1, err := s.Base.RoundTrip(req)
-	if !attemptUnauthorized || err == nil {
-		return res1, err
+	if !attemptUnauthorized {
+		return res1, errors.Join(err, signErr)
 	}
 
 	// NOTE(marius): if we received an actual error we try the request again, but unsigned.
@@ -62,21 +68,34 @@ func (s *HTTPSignatureTransport) RoundTrip(req *http.Request) (*http.Response, e
 	// I detailed that faulty behaviour in ticket:
 	//  https://todo.sr.ht/~mariusor/go-activitypub/301
 	req = cloneRequest(or) // per RoundTripper contract
-	res2, err := s.Base.RoundTrip(req)
-	if err != nil {
-		return res1, err
+	res1, err = s.Base.RoundTrip(req)
+	return res1, errors.Join(err, signErr)
+}
+
+func toCryptoPublicKey(key vocab.PublicKey) (crypto.PublicKey, error) {
+	block, _ := pem.Decode([]byte(key.PublicKeyPem))
+	if block == nil {
+		return nil, errors.Errorf("invalid PEM decode on public key")
 	}
-	return res2, nil
+	return x509.ParsePKIXPublicKey(block.Bytes)
+}
+
+func keyMismatchErr(err error) error {
+	return errors.Annotatef(err, "unable to sign request, there's a mismatch between the Actor's public and private key")
 }
 
 func (s *HTTPSignatureTransport) signRequest(req *http.Request) error {
 	if !s.Actor.PublicKey.ID.IsValid() {
-		return errors.Newf("unable to sign request, invalid Actor key ID")
+		return errors.Newf("unable to sign request, invalid Actor public key ID")
 	}
 
 	keyID, err := s.Actor.PublicKey.ID.URL()
 	if err != nil {
-		return errors.Annotatef(err, "unable to sign request, Actor key ID is not a valid URL")
+		return errors.Annotatef(err, "unable to sign request, Actor public key ID is not a valid URL")
+	}
+	actorPubKey, err := toCryptoPublicKey(s.Actor.PublicKey)
+	if err != nil {
+		return errors.Annotatef(err, "unable to sign request, Actor public key type %T is invalid", actorPubKey)
 	}
 
 	headers := headersToSign
@@ -84,19 +103,31 @@ func (s *HTTPSignatureTransport) signRequest(req *http.Request) error {
 	if req.Body != nil {
 		if _, err := io.Copy(&bodyBuf, req.Body); err == nil {
 			req.Body = io.NopCloser(&bodyBuf)
+			headers = append(headersToSign, "digest")
 		}
-
-		headers = append(headersToSign, "digest")
 	}
 
 	algos := make([]httpsig.Algorithm, 0)
-	switch s.Key.(type) {
+	switch prv := s.Key.(type) {
 	case *rsa.PrivateKey:
 		algos = append(algos, httpsig.RSA_SHA256, httpsig.RSA_SHA512)
+		if !prv.PublicKey.Equal(actorPubKey) {
+			return keyMismatchErr(err)
+		}
 	case *ecdsa.PrivateKey:
 		algos = append(algos, httpsig.ECDSA_SHA512, httpsig.ECDSA_SHA256)
+		if !prv.PublicKey.Equal(actorPubKey) {
+			return keyMismatchErr(err)
+		}
 	case ed25519.PrivateKey:
 		algos = append(algos, httpsig.ED25519)
+		if pubBytes, ok := prv.Public().([]byte); ok {
+			if actorPubBytes, ok := actorPubKey.([]byte); ok {
+				if !bytes.Equal(actorPubBytes, pubBytes) {
+					return keyMismatchErr(err)
+				}
+			}
+		}
 	}
 
 	// NOTE(marius): The only http-signatures accepted by Mastodon instances is "Signature", not "Authorization"
