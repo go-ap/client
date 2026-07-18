2cabce6028859ce840d4d756d2aa25a221c1713f
Marius Orcsik <marius@federated.id>
2026-07-18 19:23:48 +0200

    Improve the c2s sign function

--- c2s/signer.go
+++ c2s/signer.go
@@ -3,13 +3,19 @@ package c2s
 import (
 	"net/http"
 
+	"github.com/go-ap/errors"
 	"golang.org/x/oauth2"
 )
 
 type BearerSigner oauth2.Token
 
-func (b BearerSigner) Sign(r *http.Request) error {
-	t := oauth2.Token(b)
-	t.SetAuthHeader(r)
+func (b *BearerSigner) Sign(r *http.Request) error {
+	if b.TokenType == "" {
+		return errors.Newf("invalid token type")
+	}
+	if b.AccessToken == "" {
+		return errors.Newf("invalid access token")
+	}
+	r.Header.Set("Authorization", b.TokenType+" "+b.AccessToken)
 	return nil
 }
